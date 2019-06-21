c2d9ae0e488d49c7cd2374472d36432bec83397e
vergarajames <ver18002@byui.edu>
2019-06-21 16:05:11 -0500

    commit

--- css3/main_css3.js
+++
@@ -1,9 +0,0 @@
-/* function used in button */
-function changeBG(el, clr) {
-    var elem = document.getElementById(el);
-    elem.style.transtion = "background 1.0s linear 0s";
-    /*    elem.style.transtion = "background 1.0s ease-in 0s"; 
-    /*    elem.style.transtion = "background 1.0s ease-out 0s";
-    */
-    elem.style.background = clr;
-}
