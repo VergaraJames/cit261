03bbe1f6300ee98bea5315a8c6fde39982aaf452
vergarajames <ver18002@byui.edu>
2019-07-13 20:06:12 -0500

    commit

--- localstrg_05/localstorage.js
+++ localstrg_05/localstorage.js
@@ -8,7 +8,7 @@ function clickCounter() {
 
         document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
     } else {
-        document.getElementById("result").innerHTML = "Sorry, time to upgrade your browser";
+        document.getElementById("result").innerHTML = "Sorry, please upgrade your browser";
     }
 }
 
@@ -30,7 +30,7 @@ function populatePerson() {
     localStorage.setItem('browser-version', navigator.appVersion);
     localStorage.setItem('browser-cookies', navigator.cookieEnabled);
 
-    document.getElementById("userOutput").innerHTML = "Your submission is complete";
+    document.getElementById("userOutput").innerHTML = "Your information was stored";
 }
 
 function setUser() {
@@ -39,5 +39,5 @@ function setUser() {
     var browserVersion = localStorage.getItem('browser-version');
     var browserCookies = localStorage.getItem('browser-cookies');
 
-    document.getElementById("userOutput").innerHTML = "<b>First Name:</b> " + user.firstName + "<br><b>Last Name:</b> " + user.lastName + "<br><b>Email:</b> " + user.email + "<br><b>Phone:</b> " + user.phone + "<br><b>Browser Name:</b> " + browserName + "<br><b>Browser Version:</b> " + browserVersion + "<br><b>Browser Cookies Enabled:</b> " + browserCookies;
+    document.getElementById("userOutput").innerHTML = "<b>User First Name:</b> " + user.firstName + "<br><b>User Last Name:</b> " + user.lastName + "<br><b>User Email:</b> " + user.email + "<br><b>User Phone:</b> " + user.phone + "<br><b>Browser Name:</b> " + browserName + "<br><b>Browser Version:</b> " + browserVersion + "<br><b>Browser Cookies (Enabled):</b> " + browserCookies;
 }
