a54bb8ab39a23428cb1c2290c850083490447960
vergarajames <ver18002@byui.edu>
2019-09-21 22:38:33 -0500

    commit

--- css_07/main_07.js
+++ css_07/main_07.js
@@ -2,8 +2,5 @@
 function changeBG(el, clr) {
     var elem = document.getElementById(el);
     elem.style.transtion = "background 1.0s linear 0s";
-    /*    elem.style.transtion = "background 1.0s ease-in 0s"; 
-    /*    elem.style.transtion = "background 1.0s ease-out 0s";
-    */
     elem.style.background = clr;
 }
